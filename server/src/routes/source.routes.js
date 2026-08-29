b180aa19a6a88d2e0a7c214133d1c0217ccba85c
sarvagya-tech <sarvagyam93@gmail.com>
2026-08-29 23:54:15 +0530

    feat: add route handlers for memory, source, and workspace management

---
+++ server/src/routes/source.routes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { asyncHandler } from "../utils/async-handler.js";
+import {
+    bulkDeleteSources,
+    createSource,
+    deleteSource,
+    getSource,
+    importWebsite,
+    importYoutube,
+    listSources,
+    uploadPdf,
+} from "../controllers/source.controller.js";
+import { uploadSinglePdf } from "../middleware/upload.middleware.js";
+
+export const sourceRoutes = Router({ mergeParams: true });
+
+sourceRoutes.post(
+    "/upload",
+    uploadSinglePdf,
+    asyncHandler(uploadPdf),
+);
+sourceRoutes.post("/import/website", asyncHandler(importWebsite));
+sourceRoutes.post("/import/youtube", asyncHandler(importYoutube));
+sourceRoutes.get("/", asyncHandler(listSources));
+sourceRoutes.post("/", asyncHandler(createSource));
+sourceRoutes.post("/bulk-delete", asyncHandler(bulkDeleteSources));
+sourceRoutes.get("/:sourceId", asyncHandler(getSource));
+sourceRoutes.delete("/:sourceId", asyncHandler(deleteSource));
